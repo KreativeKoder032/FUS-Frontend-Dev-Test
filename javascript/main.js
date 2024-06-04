981db5906a8c27b4ddd4e189b4568e52e0e446f2
Koehler, Niklas <nkoehler001@student.franciscan.edu>
2024-06-04 17:41:12 -0400

    First Prototype Completed

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,18 @@
-const client = new XMLHttpRequest();
-client.open("GET", "/cheese-ipsum.txt");
-client.onreadystatechange = function() {
-  if (client.readyState === 4) {
-    const fileContents = client.responseText;
-    const firstSentence = fileContents.slice(end=fileContents.indexOf("."))
-    const restOfFirstParagraph = fileContents.slice(start=fileContents.indexOf(".")+1, end=fileContents.indexOf("\n") - 1)
-    document.getElementsByClassName("text-content")[0].innerHTML =
-      `<p class="first-paragraph">
-        <span class="first-sentence">
-          ${firstSentence}
-        </span>
-        ${restOfFirstParagraph}
-      </p>
-      <p class="second-paragraph">
-        ${fileContents.slice(fileContents.indexOf("\n") + 1)}
+window.onload = AddText;
+
+function AddText() {
+  const fileContents = `I love cheese, especially monterey jack pecorino. Goat cheddar cheesy feet say cheese hard cheese stilton jarlsberg mascarpone.
+  Jarlsberg fromage jarlsberg everyone loves cheese triangles emmental cheddar cut the cheese. Cheese strings fromage cheesy feet queso manchego cheese and biscuits cut the cheese fromage. Halloumi chalk and cheese.`
+  const firstSentence = fileContents.slice(start=0, end=fileContents.indexOf(".") + 1)
+  const restOfFirstParagraph = fileContents.slice(start=fileContents.indexOf(".") + 1, end=fileContents.indexOf("\n"))
+  document.getElementsByClassName("text-content")[0].innerHTML =
+    `<p class="first-paragraph">
+      <span class="first-sentence">
+        ${firstSentence}
+      </span>
+      ${restOfFirstParagraph}
+    </p>
+    <p class="second-paragraph">
+      ${fileContents.slice(fileContents.indexOf("\n") + 1)}
       </p>`
-  }
-  client.send();
 }
